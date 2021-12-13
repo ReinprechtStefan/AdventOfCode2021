a2750c5fbcc7fc927d710f4db6926d015a2fb673
Stefan Reinprecht <stefan.reinprecht@broadcom.com>
2021-12-13 18:54:31 +0100

    Advent of Code - Day 8 (Seven Segment) Part 2

--- 08/seven-segment_part2.py
+++ 08/seven-segment_part2.py
@@ -1,17 +1,43 @@
 with open('input.txt') as f:
-    lines = f.readlines()
 
-    counter = 0
+    sum = 0
 
-    for line in lines:
-        right_part = line.split(" | ")[1]
+    for line in f.readlines():
+        input_data, output = line.split('|')
+        #print(input_data, output)
 
-        for segment in right_part.strip().split(" "):
-            #print(segment, len(segment))
-            if len(segment) in [2,3,4,7]:
-                counter += 1
-            #else:
-                #print("NO ", segment, len(segment))
+        input_data_map = {}
+        for string in input_data.split():
+            input_data_map[len(string)] = set(string)
 
+        #print(input_data_map)
 
-    print(counter)
+        translated_number = ''
+        for o in map(set, output.split()):
+            #print(o)
+
+            if len(o) == 2:
+                translated_number += '1'
+            elif len(o) == 3:
+                translated_number += '7'
+            elif len(o) == 4:
+                translated_number += '4'
+            elif len(o) == 7:
+                translated_number += '8'
+            elif len(o) == 5 and len(o&input_data_map[4]) == 2:
+                translated_number += '2'
+            elif len(o) == 5 and len(o&input_data_map[4]) == 3 and len(o&input_data_map[2]) == 1:
+                translated_number += '5'
+            elif len(o) == 5 and len(o&input_data_map[4]) == 3 and len(o&input_data_map[2]) == 2:
+                translated_number += '3'
+            elif len(o) == 6 and len(o&input_data_map[4]) == 4:
+                translated_number += '9'
+            elif len(o) == 6 and len(o&input_data_map[4]) == 3 and len(o&input_data_map[2]) == 1:
+                translated_number += '6'
+            elif len(o) == 6 and len(o&input_data_map[4]) == 3 and len(o&input_data_map[2]) == 2:
+                translated_number += '0'
+            
+        print(translated_number)
+        sum += int(translated_number)
+    
+    print(sum)
